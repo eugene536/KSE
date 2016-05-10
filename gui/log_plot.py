461452834850cf13ee7ccafab9d3cb5619093520
Eugene Nemchenko <n.eugene536@gmail.com>
2016-05-10 17:13:07 +0300

    add subtitle

--- gui/log_plot.py
+++ gui/log_plot.py
@@ -3,18 +3,22 @@
 import matplotlib.pyplot as plt
 import argparse
 import sys
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--yName", help="Name of y axis", required=True, type=str)
 parser.add_argument("--xName", help="Name of x axis", required=False, type=str, default="T (temperature)")
+parser.add_argument("--plotName", help="Name of plot", required=False, type=str)
 
 args = parser.parse_args()
 plt.ylabel(args.yName)
 plt.xlabel(args.xName)
 
+plt.suptitle(args.plotName if args.plotName else args.yName, fontsize=14, fontweight='bold')
+
 xs = map(float, sys.stdin.readline().split())
 ys = map(float, sys.stdin.readline().split())
 plt.plot(xs, ys)
 
-plt.savefig(args.yName + ".png")
-#plt.show()
+#plt.savefig(args.yName + ".png")
+plt.show()
