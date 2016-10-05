242012d3804b62a8558054cfee88bf4c98ab32ce
izban <izban@mail.ru>
2016-10-05 19:09:07 +0300

    lab 1 v0.3

--- term7/main.py
+++ term7/main.py
@@ -18,26 +18,25 @@ btn4.grid(row = 1, column = 4)
 
 lbl1 = Label(root, text="x0")
 lbl1.grid(row = 2, column = 1)
-scl1 = Scale(root, from_=0, to=30, resolution=0.1)
+scl1 = Scale(root, from_=0, to=10, resolution=1)
 scl1.set(1)
 scl1.grid(row = 2, column = 2)
 
-
 lbl2 = Label(root, text="y0")
 lbl2.grid(row = 3, column = 1)
-scl2 = Scale(root, from_=0, to=30, resolution=0.1)
+scl2 = Scale(root, from_=0, to=10, resolution=1)
 scl2.set(2)
 scl2.grid(row = 3, column = 2)
 
 lbl3 = Label(root, text="z0")
 lbl3.grid(row = 4, column = 1)
-scl3 = Scale(root, from_=0, to=30, resolution=0.1)
+scl3 = Scale(root, from_=0, to=10, resolution=1)
 scl3.set(3)
 scl3.grid(row = 4, column = 2)
 
 lbl4 = Label(root, text="r")
 lbl4.grid(row = 5, column = 1)
-scl4 = Scale(root, from_=0, to=30, resolution=0.1)
+scl4 = Scale(root, from_=0, to=10, resolution=1)
 scl4.set(4)
 scl4.grid(row = 5, column = 2)
 
@@ -46,11 +45,14 @@ def getStartPoint():
     return scl1.get(), scl2.get(), scl3.get(), scl4.get()
 
 def makeWork(algorithm):
-    def work():
+    def work(*_):
         Plots.drawPlots(algorithm(scl1.get(), scl2.get(), scl3.get(), scl4.get()))
     return work
 
 
 btn1.config(command=makeWork(Euler.solve))
-
+scl1.config(command=makeWork(Euler.solve))
+#scl2.config(command=makeWork(Euler.solve))
+#scl3.config(command=makeWork(Euler.solve))
+#scl4.config(command=makeWork(Euler.solve))
 root.mainloop()
