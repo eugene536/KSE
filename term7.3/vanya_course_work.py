01e150411cfe8bac4f1660cf49ac89259ce0b57d
belonogov ivan <belonogov95@gmail.com>
2016-12-21 14:59:18 +0300

    done

--- term7.3/vanya_course_work.py
+++ term7.3/vanya_course_work.py
@@ -31,8 +31,10 @@ gAlf = 0
 gR = 0
 gE = 0
 
+
 def getW(x, T):
     return getSpeed(x, T, gK, gAlf, gR, gE)
+
 
 def initT(cntH, Tm, T0):
     res = [Tm]
@@ -66,17 +68,17 @@ def calc(Tm, T0, H, cntH, totalTime, cntTime, ro, C, D, lambd, Q, K, R, E, alf):
 
     dz = H * 1.0 / cntH
     dt = totalTime * 1.0 / cntTime
-    mxT = totalTime
+    mxT = cntTime
     mxH = cntH
 
-    for n in range(0, totalTime - 1):
+    for n in range(0, cntTime - 1):
         for k in range(1, cntH - 1):
             tableX[n + 1][k] = dt * (D * (tableX[n][k - 1] - 2 * tableX[n][k] + tableX[n][k + 1]) / dz / dz + getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E)) + tableX[n][k]
             tableT[n + 1][k] = dt * (lambd / (ro * C) * (tableT[n][k - 1] - 2 * tableT[n][k] + tableT[n][k + 1]) / dz / dz - Q / C * getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E)) + tableT[n][k]
         tableX[n + 1][0] = 0
-        tableX[n + 1][cntH - 1] = 0
+        tableX[n + 1][cntH - 1] = tableX[n + 1][cntH - 2]
         tableT[n + 1][0] = Tm
-        tableT[n + 1][cntH - 1] = T0
+        tableT[n + 1][cntH - 1] = tableT[n + 1][cntH - 2]
 
 calc(300, 100, 10, 100, 10, 100, 10, 10, 10, 10, 10, 10, 10, 10, 10)
 
