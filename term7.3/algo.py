72ccb39429ba8dc57c6da4c3a14a3d8a651927a6
belonogov ivan <belonogov95@gmail.com>
2016-12-21 15:42:35 +0300

    asdf

--- term7.3/algo.py
+++ term7.3/algo.py
@@ -13,6 +13,8 @@ def getX(t, z):
     idZ = int(z / dz)
     idT = min(mxT - 1, idT)
     idZ = min(mxH - 1, idZ)
+    print (dt, dz)
+    print (idT, idZ)
     return tableX[idT][idZ]
 
 def getT(t, z):
@@ -75,15 +77,32 @@ def calc(Tm, T0, H, cntH, totalTime, cntTime, ro, C, D, lambd, Q, K, R, E, alf):
     for n in range(0, cntTime - 1):
         for k in range(1, cntH - 1):
             tableX[n + 1][k] = dt * (D * (tableX[n][k - 1] - 2 * tableX[n][k] + tableX[n][k + 1]) / dz / dz + getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E)) + tableX[n][k]
-            tableT[n + 1][k] = dt * (lambd / (ro * C) * (tableT[n][k - 1] - 2 * tableT[n][k] + tableT[n][k + 1]) / dz / dz - Q / C * getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E)) + tableT[n][k]
+            tableT[n + 1][k] = dt * (lambd * 1.0 / (ro * C) * (tableT[n][k - 1] - 2 * tableT[n][k] + tableT[n][k + 1]) / dz / dz - Q * 1.0 / C * getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E)) + tableT[n][k]
+            # print(dt, dz)
+            # print (D)
+            # print ("der ", (tableX[n][k - 1] - 2 * tableX[n][k] + tableX[n][k + 1]))
+            #
+            # print (" cof " , D * (tableX[n][k - 1] - 2 * tableX[n][k] + tableX[n][k + 1]) / dz / dz, getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E))
+            # print ("x t", tableX[n + 1][k], tableT[n + 1][k])
+            # print ("her")
+            # print(dt)
+
+            # print ((tableT[n][k - 1] - 2 * tableT[n][k] + tableT[n][k + 1]))
+            # print("temp: ",  lambd * 1.0 / (ro * C) * (tableT[n][k - 1] - 2 * tableT[n][k] + tableT[n][k + 1]) / dz / dz,  - Q / C * getSpeed(tableX[n][k], tableT[n][k], K, alf, R, E))
+
+            # return
         tableX[n + 1][0] = 0
         tableX[n + 1][cntH - 1] = tableX[n + 1][cntH - 2]
         tableT[n + 1][0] = Tm
         tableT[n + 1][cntH - 1] = tableT[n + 1][cntH - 2]
 
-calc(Constants.Tm, Constants.T0, Constants.H,
-     Constants.cntH, Constants.totalTime, Constants.cntTime, Constants.ro, Constants.C, Constants.D, Constants.lam, Constants.Q, Constants.K, Constants.R, Constants.E, Constants.alpha)
-print (getX(1000, 10))
+
+
+calc(Constants.Tm, Constants.T0, Constants.H, Constants.cntH, Constants.totalTime, Constants.cntTime, Constants.ro, Constants.C, Constants.D, Constants.lam, Constants.Q, Constants.K, Constants.R, Constants.E, Constants.alpha)
+# print(getSpeed(1, Constants.T0, Constants.K, Constants.alpha, Constants.R, Constants.E))
+
+print ("x ", getX(10, 0.1))
+print ("temp ", getT(30, 0.1))
 
 print("heelo")
 
